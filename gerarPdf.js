61f25c0b23e172b15d15b4e7c93d0db35007cd41
Perkles <fortesvitoria@gmail.com>
2024-04-16 21:00:12 -0300

    tentar gerar pdf

---
+++ gerarPdf.js
@@ -0,0 +1,19 @@
+const btnGerar = document.querySelector("#imprimirPdf");
+
+btnGerar.addEventListener("click", () => {
+
+    // conteudo pdf
+    const conteudo = document.querySelector("#conteudo");
+
+    // configurar pdf
+    const options = {
+        margin: [10,10,10,10],
+        filename: "arquivo.pdf",
+        html2canvas:  {scale:2},
+        jsPDF: {unit:mm}, format: "a4", orientation:"portrait"
+    };
+
+    // gerar e baixar pdf
+    html2pdf().set(options).format(conteudo).save();
+
+})
